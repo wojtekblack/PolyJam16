09e78e41d3b769ab406e907a790f47f98f823790
Tomek Czarny <ecfatum@gmail.com>
2016-01-31 14:34:49 +0100

    gameplay

--- assets/scripts/ui.lua
+++ assets/scripts/ui.lua
@@ -1,11 +1,35 @@
 require "assets/scripts/monsters"
+require "assets/scripts/collectable"
 
 ui = {}
 
+type1 = 0
+type2 = 0
+
+function randTypes()
+  type1 = love.math.random( 4 )
+  type2 = love.math.random( 4 )
+end
+
+ui.addNeutralCollectible = function( collectibleType )
+  randTypes();
+end
+
 ui.addCollectible = function( collectibleType, playerIndex )
-  if collectibleType == 0 or collectibleType == 1 then
+  if playerIndex == 0 and collectibleType == type1 then
     monsters:hit1()
-  elseif collectibleType == 2 or collectibleType == 3 then
+  elseif playerIndex == 1 and collectibleType == type2 then
     monsters:hit2()
   end
+  
+  randTypes();
 end
+    
+function ui:load()
+  randTypes()
+end
+    
+function ui:draw()
+  collectable.drawUI( type1, type2 )
+end
+    
